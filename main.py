47a7d73a21e2178817bf607cb8d0814989d26624
darmyn <81786165+darmyn@users.noreply.github.com>
2021-10-18 02:56:51 -0400

    Avoid NonScriptable Props
    Properties that are given the `NonScriptable` tag are ignored from the API dump.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from os import read, write
 import requests
 import json
 
@@ -5,15 +6,39 @@ ok = 200
 host = "https://raw.githubusercontent.com/MaximumADHD/Roblox-Client-Tracker/roblox/API-Dump.json"
 result = requests.get(host)
 
+temp_repeated_store = {}
+
 if result.status_code == ok:
     restructure = {}
     api = result.json()
     for rbxClass in api["Classes"]:
         properties = []
-        for property in rbxClass["Members"]:
-            if property["MemberType"] == "Property":
-                properties.append(property["Name"])
-        restructure[rbxClass["Name"]] = properties
+        methods = []
+        for member in rbxClass["Members"]:
+            #for key in member.keys():
+            #    if not temp_repeated_store.get(key):
+            #        temp_repeated_store[str(key)] = True
+            #        print(key)
+            #tags = member.get("Tags")
+            #if tags:
+            #    print(tags)
+
+            tags = member.get("Tags")
+            ## print(security)
+
+            if tags:
+                if "NotScriptable" in tags:
+                    print(member["Name"] + ": " + rbxClass["Name"])
+                    continue
+            if member["MemberType"] == "Property":
+                properties.append(member["Name"])
+            if member["MemberType"] == "Method":
+                methods.append(member["Name"])
+            name = rbxClass["Name"]
+        restructure[name] = {
+            "properties": properties,
+            "methods": methods
+        }
     with open("dump.json", "w") as dump:
         dump.write(json.dumps(restructure))
         dump.close()
